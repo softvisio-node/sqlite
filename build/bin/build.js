bd0e7db2a23d982714f1b83d4ed587e0cc755f56
zdm <zdm@softvisio.net>
2022-01-01 15:12:56 +0200

    feat: build on github

--- build/bin/build.js
+++ build/bin/build.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
 
-console.log( process.env );
+import { resolve } from "#core/utils";
+import path from "path";
+import childProcess from "child_process";
+
+// find better-sqlit3 location
+const cwd = path.dirname( resolve( "better-sqlite3/package.json", import.meta.url ) );
+
+// install better-sqlite3 deps
+childProcess.spawnSync( "npm", ["i", "--ignore-scripts"], { cwd, "shell": true, "stdio": "inherit" } );
+
+// XXX update sqlite sources
+
+// patch
+childProcess.spawnSync( "sed", ["-i", "-e", "'/SQLITE_USE_URI=0/ s/=0/=1/'", "deps/defines.gypi"], { cwd, "shell": true, "stdio": "inherit" } );
+
+// build for current nodejs version
+childProcess.spawnSync( "npx", ["--no-install", "prebuild", "--strip", "--include-regex", "better_sqlite3.node$", "-r", "node"], { cwd, "shell": true, "stdio": "inherit" } );
+
+// build for current electron version
+// childProcess.spawnSync( "npx", ["--no-install", "prebuild", "--strip", "--include-regex", "better_sqlite3.node$", "-r", "electron", "-t", "16.0.0"], { cwd, "shell": true, "stdio": "inherit" } );
