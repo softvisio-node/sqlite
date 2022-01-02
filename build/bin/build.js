0657742e12b4966b72dd49953d4a79c306f82ff4
zdm <zdm@softvisio.net>
2022-01-02 12:21:52 +0200

    feat: build on github

--- build/bin/build.js
+++ build/bin/build.js
@@ -6,6 +6,10 @@ import childProcess from "child_process";
 import GitHubApi from "#core/api/github";
 import glob from "#core/glob";
 import File from "#core/file";
+import AdmZip from "adm-zip";
+import tarStream from "tar-stream";
+import zlib from "zlib";
+import fs from "fs";
 
 const REPO = "softvisio/sqlite";
 const TAG = "v1.9.0";
@@ -17,14 +21,10 @@ const sqliteUrl = "https://www.sqlite.org/2021/sqlite-amalgamation-3370100.zip";
 const cwd = path.dirname( resolve( "better-sqlite3/package.json", import.meta.url ) );
 
 // install better-sqlite3 deps
-// childProcess.spawnSync( "npm", ["i", "--ignore-scripts"], { cwd, "shell": true, "stdio": "inherit" } );
+childProcess.spawnSync( "npm", ["i", "--ignore-scripts"], { cwd, "shell": true, "stdio": "inherit" } );
 
-// XXX update sqlite sources
-childProcess.spawnSync( "curl", ["-fsSLo", "deps/sqlite3.zip", sqliteUrl], { cwd, "shell": true, "stdio": "inherit" } );
-childProcess.spawnSync( "unzip", ["-j", "deps/sqlite3.zip", "-d", "sqlite"], { cwd, "shell": true, "stdio": "inherit" } );
-childProcess.spawnSync( "tar", ["cvfz", "deps/sqlite.tar.gz", "sqlite"], { cwd, "shell": true, "stdio": "inherit" } );
-
-process.exit();
+// update sqlite sources
+await updateSqlite();
 
 // patch
 childProcess.spawnSync( "sed", ["-i", "-e", "'/SQLITE_USE_URI=0/ s/=0/=1/'", "deps/defines.gypi"], { cwd, "shell": true, "stdio": "inherit" } );
@@ -45,4 +45,27 @@ for ( const file of glob( "prebuilds/*.tar.gz", { cwd, "sync": true } ) ) {
 
     const res = await gitHubApi.updateReleaseAsset( REPO, release.data.id, new File( { "path": path.join( cwd, file ), name } ) );
     if ( !res.ok ) process.exit( 1 );
+}
+
+async function updateSqlite () {
+    childProcess.spawnSync( "curl", ["-fsSLo", "deps/sqlite3.zip", sqliteUrl], { cwd, "shell": true, "stdio": "inherit" } );
+
+    const zip = new AdmZip( path.join( cwd, "deps/sqlite3.zip" ) ),
+        tar = tarStream.pack();
+
+    zip.getEntries().forEach( f => {
+        if ( !f.name ) return;
+
+        tar.entry( { "name": f.name }, f.getData() );
+    } );
+
+    tar.finalize();
+
+    const out = fs.createWriteStream( path.join( cwd, "deps/sqlite.tar.gz" ) );
+
+    tar.pipe( zlib.createGzip().pipe( out ) );
+
+    await new Promise( resolve => out.once( "end", resolve ) );
+
+    process.exit();
 }
