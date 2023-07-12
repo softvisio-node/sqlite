ca3f9b846fb0ab44e30f72a3366ac173062eda5f
zdm <zdm@softvisio.net>
2023-07-12 12:02:47 +0300

    fix: resource location

--- bin/install.mjs
+++ bin/install.mjs
@@ -2,11 +2,11 @@
 
 import externalResources from "@softvisio/external-resources";
 
-externalResources.add( `softvisio-node/sqlite/resources/node-v${process.versions.modules}-${process.platform}-${process.arch}.node`, import.meta.url, { "location": "lib/binaries" } );
+externalResources.add( `softvisio-node/sqlite/resources/node-v${process.versions.modules}-${process.platform}-${process.arch}.node`, { "location": "lib/binaries", "resolve": import.meta.url } );
 
 // under windows download linux binaries for vmware
 if ( process.platform === "win32" ) {
-    externalResources.add( `softvisio-node/sqlite/resources/node-v${process.versions.modules}-linux-${process.arch}.node`, import.meta.url, { "location": "lib/binaries" } );
+    externalResources.add( `softvisio-node/sqlite/resources/node-v${process.versions.modules}-linux-${process.arch}.node`, { "location": "lib/binaries", "resolve": import.meta.url } );
 }
 
 const res = await externalResources.update( {
