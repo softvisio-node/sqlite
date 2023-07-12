cdaed03c556eb48a7d0b7537dad978b5e53a4c2d
zdm <zdm@softvisio.net>
2023-07-12 06:04:45 +0300

    fix: resources

--- bin/install.mjs
+++ bin/install.mjs
@@ -1,46 +1,18 @@
 #!/usr/bin/env node
 
-import fs from "node:fs";
-import { Readable } from "node:stream";
-import { pipeline } from "node:stream/promises";
-import zlib from "node:zlib";
+import externalResources from "@softvisio/external-resources";
 
-const url = new URL( "https://github.com/softvisio-node/sqlite/releases/download/data/" );
-
-await get( url, `node-v${process.versions.modules}-${process.platform}-${process.arch}.node` );
+externalResources.add( `softvisio-node/sqlite/resources/node-v${process.versions.modules}-${process.platform}-${process.arch}.node`, import.meta.url, { "location": "lib/binaries" } );
 
 // under windows download linux binaries for vmware
-if ( process.platform === "win32" ) await get( url, `node-v${process.versions.modules}-linux-${process.arch}.node` );
-
-async function get ( url, file ) {
-    process.stdout.write( `Downloading: ${file} ... ` );
-
-    try {
-        var res = await fetch( url + file + ".gz" );
-    }
-    catch ( e ) {
-        console.log( e + "" );
-
-        process.exit( 1 );
-    }
-
-    if ( !res.ok ) {
-        console.log( res.status + " " + res.statusText );
-
-        process.exit( 1 );
-    }
-    else {
-        fs.mkdirSync( "lib/binaries", { "recursive": true } );
-
-        const e = await pipeline( Readable.fromWeb( res.body ), zlib.createGunzip(), fs.createWriteStream( `lib/binaries/${file}` ) ).catch( e => e );
-
-        if ( e ) {
-            console.log( e + "" );
-
-            process.exit( 1 );
-        }
-        else {
-            console.log( "OK" );
-        }
-    }
+if ( process.platform === "win32" ) {
+    externalResources.add( `softvisio-node/sqlite/resources/node-v${process.versions.modules}-linux-${process.arch}.node`, import.meta.url, { "location": "lib/binaries" } );
 }
+
+const res = await externalResources.update( {
+    "remote": true,
+    "force": false,
+    "silent": false,
+} );
+
+if ( !res.ok ) process.exit( 1 );
