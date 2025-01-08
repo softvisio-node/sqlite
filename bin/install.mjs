dd56fa4cab0b540eac30434896f17d1ac11e6df5
zdm <zdm@softvisio.net>
2025-01-08 21:03:12 +0200

    chore: fix external resource id

--- bin/install.mjs
+++ bin/install.mjs
@@ -5,6 +5,7 @@ import externalResources from "@softvisio/utils/external-resources";
 externalResources.add( {
     "id": "softvisio-node/sqlite",
     "node": true,
+    "caller": import.meta.url,
 } );
 
 const res = await externalResources.install( {
