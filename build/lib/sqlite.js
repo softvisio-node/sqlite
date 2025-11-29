5d52fd6645285d893f98a1d98bace66d07dc8a1b
zdm <zdm@softvisio.net>
2025-11-29 07:42:55 +0200

    build(deps): bump sqlite@3.51.1

--- build/lib/sqlite.js
+++ build/lib/sqlite.js
@@ -9,7 +9,7 @@ import Zip from "#core/zip";
 
 const USE_LATEST_SQLITE = true,
     SQLITE_DOMAIN = "www3.sqlite.org",
-    SQLITE_VERSION = "3.51.0",
+    SQLITE_VERSION = "3.51.1",
     SQLITE_YEAR = 2025,
     SQLITE_PRODUCT_VERSION =
         SQLITE_VERSION.split( "." )
